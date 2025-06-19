6cc11a02bd01dba91a7fbf975b9a36c31e216fbc
AnithaD1625 <anithadhasarathan22@gmail.com>
2025-06-19 22:47:21 +0530

    Update Singleton.java

---
+++ ThreadSafeSingleton/Singleton.java
@@ -0,0 +1,30 @@
+package ThreadSafeSingleton;
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() { }
+
+    public static synchronized Singleton getInstance() {  // Synchronization added
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            Singleton instance = Singleton.getInstance();
+            System.out.println(Thread.currentThread().getName() + " → " + instance.hashCode());
+        };
+
+        // Start two threads at the same time
+        Thread thread1 = new Thread(task, "Thread-1");
+        Thread thread2 = new Thread(task, "Thread-2");
+
+        thread1.start();
+        thread2.start();
+    }
+}
