066fae04d436f708794ad9019e743cae624e44ec
AnithaD1625 <anithadhasarathan22@gmail.com>
2025-07-06 22:17:47 +0530

    Add files via upload

---
+++ Week3_Spring-Data-JPA/SpringDataJPA-QuickExample/Code/spring-data-jpa-handson/src/main/java/com/sjprogramming/restapi/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.sjprogramming.restapi.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sjprogramming.restapi.model.User;
+import com.sjprogramming.restapi.repository.UserRepository;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
