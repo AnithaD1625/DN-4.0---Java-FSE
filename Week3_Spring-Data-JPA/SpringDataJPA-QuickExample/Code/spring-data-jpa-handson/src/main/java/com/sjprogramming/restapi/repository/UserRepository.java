1d1a6c81a500c350f5e79098faf34e5c625c0bfa
AnithaD1625 <anithadhasarathan22@gmail.com>
2025-07-08 11:18:21 +0530

    commit

--- Week3_Spring-Data-JPA/SpringDataJPA-QuickExample/Code/spring-data-jpa-handson/src/main/java/com/sjprogramming/restapi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sjprogramming.restapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sjprogramming.restapi.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
